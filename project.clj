f605c9ccfd09de5e864b732c57e8ddeb7859dc05
Michael du Breuil <wicked.shell.scripts@gmail.com>
2018-01-12 21:15:10 -0700

    Update to 1.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject tools-timer "1.0.3"
+(defproject tools-timer "1.0.4-SNAPSHOT"
   :description "An easy to use Java Timer wrapper for clojure."
   :lein-release {:deploy-via :clojars}
   :min-lein-version "2.0.0"
   :url "http://github.com/wickedshell/tools-timer"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]
-                   :plugins [[codox "0.6.4"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}}
   :global-vars {*warn-on-reflection* true})
